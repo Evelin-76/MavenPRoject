f4ae466d730a85328e870343afc74730c24893eb
Evelin <evelindacanovas@gmail.com>
2021-06-06 18:13:15 +0200

    hello new commit

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -3,5 +3,7 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello");
         System.out.println("Developing hello features");
+        System.out.println("GitHub repo is already published to GitHub");
+        System.out.println("Doing more work");
     }
 }
